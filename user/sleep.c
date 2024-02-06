342d56688ae12c9bae292b26b6fb909cd2bf2704
GEJXD <2693786339@qq.com>
2024-02-06 18:27:41 +0800

    finished the lab01-sleep

--- user/sleep.c
+++ user/sleep.c
@@ -6,14 +6,15 @@
 
 int main(int argv, char *argc[]) {
     if (argv <= 1) {
-        fprintf(2, "sleep time not found.\n");
+        fprintf(2, "Argument Error: Not found any arguments!\n");
+        exit(0);
+    } else if (argv != 2) {
+        fprintf(2, "Argument Error: Too many arguments! sleep command expect "
+                   "only 1 argumens.\n");
         exit(0);
     }
 
-    int i;
-    for (i = 1; i < argv; i++) {
-        int sleepTime = atoi(argc[i]);
-        sleep(sleepTime);
-    }
+    int time = atoi(argc[1]);
+    sleep(time);
     exit(0);
 }
