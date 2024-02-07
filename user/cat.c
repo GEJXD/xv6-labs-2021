ffc9aec8b73f86dc2a0e8afba8df58447bb321c1
GEJXD <2693786339@qq.com>
2024-02-08 06:20:09 +0800

    nothing

--- user/cat.c
+++ user/cat.c
@@ -4,40 +4,36 @@
 
 char buf[512];
 
-void
-cat(int fd)
-{
-  int n;
+void cat(int fd) {
+    int n;
 
-  while((n = read(fd, buf, sizeof(buf))) > 0) {
-    if (write(1, buf, n) != n) {
-      fprintf(2, "cat: write error\n");
-      exit(1);
+    while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        if (write(1, buf, n) != n) {
+            fprintf(2, "cat: write error\n");
+            exit(1);
+        }
     }
-  }
-  if(n < 0){
-    fprintf(2, "cat: read error\n");
-    exit(1);
-  }
+    if (n < 0) {
+        fprintf(2, "cat: read error\n");
+        exit(1);
+    }
 }
 
-int
-main(int argc, char *argv[])
-{
-  int fd, i;
+int main(int argc, char *argv[]) {
+    int fd, i;
 
-  if(argc <= 1){
-    cat(0);
-    exit(0);
-  }
-
-  for(i = 1; i < argc; i++){
-    if((fd = open(argv[i], 0)) < 0){
-      fprintf(2, "cat: cannot open %s\n", argv[i]);
-      exit(1);
+    if (argc <= 1) {
+        cat(0);
+        exit(0);
     }
-    cat(fd);
-    close(fd);
-  }
-  exit(0);
+
+    for (i = 1; i < argc; i++) {
+        if ((fd = open(argv[i], 0)) < 0) {
+            fprintf(2, "cat: cannot open %s\n", argv[i]);
+            exit(1);
+        }
+        cat(fd);
+        close(fd);
+    }
+    exit(0);
 }
