033a94f8d544715dcc1e4bf07bb931522432369d
GEJXD <2693786339@qq.com>
2024-03-13 11:29:25 +0800

    start lab & format code-style

--- user/pgtbltest.c
+++ user/pgtbltest.c
@@ -7,64 +7,56 @@
 void ugetpid_test();
 void pgaccess_test();
 
-int
-main(int argc, char *argv[])
-{
-  ugetpid_test();
-  pgaccess_test();
-  printf("pgtbltest: all tests succeeded\n");
-  exit(0);
+int main(int argc, char *argv[]) {
+    ugetpid_test();
+    pgaccess_test();
+    printf("pgtbltest: all tests succeeded\n");
+    exit(0);
 }
 
 char *testname = "???";
 
-void
-err(char *why)
-{
-  printf("pgtbltest: %s failed: %s, pid=%d\n", testname, why, getpid());
-  exit(1);
+void err(char *why) {
+    printf("pgtbltest: %s failed: %s, pid=%d\n", testname, why, getpid());
+    exit(1);
 }
 
-void
-ugetpid_test()
-{
-  int i;
+void ugetpid_test() {
+    int i;
 
-  printf("ugetpid_test starting\n");
-  testname = "ugetpid_test";
+    printf("ugetpid_test starting\n");
+    testname = "ugetpid_test";
 
-  for (i = 0; i < 64; i++) {
-    int ret = fork();
-    if (ret != 0) {
-      wait(&ret);
-      if (ret != 0)
-        exit(1);
-      continue;
+    for (i = 0; i < 64; i++) {
+        int ret = fork();
+        if (ret != 0) {
+            wait(&ret);
+            if (ret != 0)
+                exit(1);
+            continue;
+        }
+        if (getpid() != ugetpid())
+            err("missmatched PID");
+        exit(0);
     }
-    if (getpid() != ugetpid())
-      err("missmatched PID");
-    exit(0);
-  }
-  printf("ugetpid_test: OK\n");
+    printf("ugetpid_test: OK\n");
 }
 
-void
-pgaccess_test()
-{
-  char *buf;
-  unsigned int abits;
-  printf("pgaccess_test starting\n");
-  testname = "pgaccess_test";
-  buf = malloc(32 * PGSIZE);
-  if (pgaccess(buf, 32, &abits) < 0)
-    err("pgaccess failed");
-  buf[PGSIZE * 1] += 1;
-  buf[PGSIZE * 2] += 1;
-  buf[PGSIZE * 30] += 1;
-  if (pgaccess(buf, 32, &abits) < 0)
-    err("pgaccess failed");
-  if (abits != ((1 << 1) | (1 << 2) | (1 << 30)))
-    err("incorrect access bits set");
-  free(buf);
-  printf("pgaccess_test: OK\n");
+void pgaccess_test() {
+    char *buf;
+    unsigned int abits;
+    printf("pgaccess_test starting\n");
+    testname = "pgaccess_test";
+    buf = malloc(32 * PGSIZE);
+    if (pgaccess(buf, 32, &abits) < 0)
+        err("pgaccess failed");
+    buf[PGSIZE * 1] += 1;
+    buf[PGSIZE * 2] += 1;
+    buf[PGSIZE * 30] += 1;
+    if (pgaccess(buf, 32, &abits) < 0)
+        err("pgaccess failed");
+    if (abits != ((1 << 1) | (1 << 2) | (1 << 30)))
+        err("incorrect access bits set");
+    free(buf);
+    printf("pgaccess_test: OK\n");
 }
--- user/stressfs.c
+++ user/stressfs.c
@@ -13,37 +13,35 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
-int
-main(int argc, char *argv[])
-{
-  int fd, i;
-  char path[] = "stressfs0";
-  char data[512];
+int main(int argc, char *argv[]) {
+    int fd, i;
+    char path[] = "stressfs0";
+    char data[512];
 
-  printf("stressfs starting\n");
-  memset(data, 'a', sizeof(data));
+    printf("stressfs starting\n");
+    memset(data, 'a', sizeof(data));
 
-  for(i = 0; i < 4; i++)
-    if(fork() > 0)
-      break;
+    for (i = 0; i < 4; i++)
+        if (fork() > 0)
+            break;
 
-  printf("write %d\n", i);
+    printf("write %d\n", i);
 
-  path[8] += i;
-  fd = open(path, O_CREATE | O_RDWR);
-  for(i = 0; i < 20; i++)
-//    printf(fd, "%d\n", i);
-    write(fd, data, sizeof(data));
-  close(fd);
+    path[8] += i;
+    fd = open(path, O_CREATE | O_RDWR);
+    for (i = 0; i < 20; i++)
+        //    printf(fd, "%d\n", i);
+        write(fd, data, sizeof(data));
+    close(fd);
 
-  printf("read\n");
+    printf("read\n");
 
-  fd = open(path, O_RDONLY);
-  for (i = 0; i < 20; i++)
-    read(fd, data, sizeof(data));
-  close(fd);
+    fd = open(path, O_RDONLY);
+    for (i = 0; i < 20; i++)
+        read(fd, data, sizeof(data));
+    close(fd);
 
-  wait(0);
+    wait(0);
 
-  exit(0);
+    exit(0);
 }
