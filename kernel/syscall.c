d2218910629711d49e6182623caee90137e1aabc
GEJXD <2693786339@qq.com>
2024-08-21 14:57:45 +0800

    Finshed first assignment

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -85,22 +85,23 @@ extern uint64 sys_uptime(void);
 extern uint64 sys_trace(void);
 
 static uint64 (*syscalls[])(void) = {
-    [SYS_fork] sys_fork,   [SYS_exit] sys_exit,     [SYS_wait] sys_wait,     [SYS_pipe] sys_pipe,
-    [SYS_read] sys_read,   [SYS_kill] sys_kill,     [SYS_exec] sys_exec,     [SYS_fstat] sys_fstat,
-    [SYS_chdir] sys_chdir, [SYS_dup] sys_dup,       [SYS_getpid] sys_getpid, [SYS_sbrk] sys_sbrk,
-    [SYS_sleep] sys_sleep, [SYS_uptime] sys_uptime, [SYS_open] sys_open,     [SYS_write] sys_write,
-    [SYS_mknod] sys_mknod, [SYS_unlink] sys_unlink, [SYS_link] sys_link,     [SYS_mkdir] sys_mkdir,
-    [SYS_close] sys_close, [SYS_trace] sys_trace,
+    [SYS_fork] sys_fork,   [SYS_exit] sys_exit,     [SYS_wait] sys_wait,
+    [SYS_pipe] sys_pipe,   [SYS_read] sys_read,     [SYS_kill] sys_kill,
+    [SYS_exec] sys_exec,   [SYS_fstat] sys_fstat,   [SYS_chdir] sys_chdir,
+    [SYS_dup] sys_dup,     [SYS_getpid] sys_getpid, [SYS_sbrk] sys_sbrk,
+    [SYS_sleep] sys_sleep, [SYS_uptime] sys_uptime, [SYS_open] sys_open,
+    [SYS_write] sys_write, [SYS_mknod] sys_mknod,   [SYS_unlink] sys_unlink,
+    [SYS_link] sys_link,   [SYS_mkdir] sys_mkdir,   [SYS_close] sys_close,
+    [SYS_trace] sys_trace,
 };
 
 // lab syscall
 // add an array of syscall names to index into
-static char* syscall_name[] = 
-    {"#",   // to make index start from 1;
-    "fork",  "exit",   "wait",  "pipe",  "read",   "kill",
-    "exec",  "fstat",  "chdir", "dup",   "getpid", "sbrk",
-    "sleep", "uptime", "open",  "write", "mknod",  "unlink",
-    "link",  "mkdir",  "close", "trace"};
+static char* syscall_name[] = {
+    "#",   // to make index start from 1;
+    "fork",  "exit",   "wait",   "pipe",  "read",  "kill",   "exec", "fstat",
+    "chdir", "dup",    "getpid", "sbrk",  "sleep", "uptime", "open", "write",
+    "mknod", "unlink", "link",   "mkdir", "close", "trace"};
 
 void syscall(void) {
     int num;
@@ -112,7 +113,10 @@ void syscall(void) {
         p->trapframe->a0 = syscalls[num]();
         uint32 mask = p->trace_mask;
         if ((mask >> num) & 1) {
-            printf("%d: syscall %s -> %d\n", p->pid, syscall_name[num], p->trapframe->a0);
+            printf("%d: syscall %s -> %d\n",
+                   p->pid,
+                   syscall_name[num],
+                   p->trapframe->a0);
         }
     } else {
         printf("%d %s: unknown sys call %d\n", p->pid, p->name, num);
